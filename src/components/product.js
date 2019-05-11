c0cb43005e6d64be26c19ee717490d3d57251ee8
Deseteral <deseteral@gmail.com>
2019-05-11 20:46:21 +0200

    Product image

--- src/components/product.js
+++ src/components/product.js
@@ -15,9 +15,14 @@ function renderProduct(product) {
   return `
   <div class="product">
     <div class="product__name">${product.name}</div>
-    <div class="product__price">${formatPrice(product.price)}</div>
-    <div class="product__description">
-      ${product.description.text}
+    <div class="product__details">
+      <img class="product__image" src="${product.image.url}"></img>
+      <div>
+        <div class="product__price">${formatPrice(product.price)}</div>
+        <div class="product__description">
+          ${product.description.text}
+        </div>
+      </div>
     </div>
   </div>
   `;
